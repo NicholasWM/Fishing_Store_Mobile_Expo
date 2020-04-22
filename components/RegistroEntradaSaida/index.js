7d970f2107a9db5b51f67c6e1a8b47935f2c36b0
NicholasWM <nicholaswesleymazzei@gmail.com>
2020-04-21 21:30:49 -0300

    refact: Compatibilidade RegistroEntradaSaida

--- components/RegistroEntradaSaida/index.js
+++ components/RegistroEntradaSaida/index.js
@@ -6,15 +6,15 @@ import {date} from '../../helpers/Date'
 
 export default function RegistroEntradaSaida({item}){
 	return (
-		<View style={item.modo == 'entrada' ? {...styles.registerContainer , flexDirection:'row-reverse', borderColor:'green'}:styles.registerContainer}>
-			<View style={item.modo == 'entrada' ? {...styles.registerBox1, borderLeftWidth:2, borderColor:'green'}:{...styles.registerBox1, borderRightWidth:2, borderColor:'red'}}>
-				<Text style={styles.registerTextBox1}>{item.quantidade} unidades</Text>
-				<Text style={styles.registerTextBox1}>{item.preco} reais</Text>
+		<View style={item.modo.toLowerCase() == 'entrada' ? {...styles.registerContainer , flexDirection:'row-reverse', borderColor:'green'}:styles.registerContainer}>
+			<View style={item.modo.toLowerCase() == 'entrada' ? {...styles.registerBox1, borderLeftWidth:2, borderColor:'green'}:{...styles.registerBox1, borderRightWidth:2, borderColor:'red'}}>
+				{item.quantidade && <Text style={styles.registerTextBox1}>{item.quantidade} unidades</Text>}
+				{item.preco && <Text style={styles.registerTextBox1}>{item.preco} reais</Text>}
 			</View>
 			<View style={styles.registerBox2}>
 				<Text style={styles.registerTextBox2}>{date(item.createdAt)}</Text>
 			</View>
-			<View style={item.modo == 'entrada' ? styles.registerBox3Entry:styles.registerBox3Exit}>
+			<View style={item.modo.toLowerCase() == 'entrada' ? styles.registerBox3Entry:styles.registerBox3Exit}>
 				<Text style={styles.registerTextBox3}>{item.modo}</Text>
 			</View>
 		</View>
