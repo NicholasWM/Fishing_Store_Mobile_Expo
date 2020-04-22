09606940964bde657cbcd38a326a542563c9b9cb
NicholasWM <nicholaswesleymazzei@gmail.com>
2020-04-22 20:39:45 -0300

    refact and bugCorretion: Bug tela branca e utilizacao do componente RegistroEntradaSaida2

--- screens/HistoricoEstoque/index.js
+++ screens/HistoricoEstoque/index.js
@@ -1,56 +1,58 @@
 
-import React, {
-    useEffect,
-} from 'react';
-
-import { Text,
-        View,
+import React, {useEffect} from 'react';
+import {View,
+        Button,
         FlatList,
-        ImageBackground
 } from 'react-native';
+
 import { useSelector, useDispatch } from 'react-redux'
-import {getImage} from '../../helpers/Image'
 import { getHistoryStock } from '../../store/fetchActions'
 import { setSearch, activateSearchAction } from '../../store/fetchActions'
-import styles from './Styles'
-import {date} from '../../helpers/Date'
+import RegistroEntradaSaida2 from '../../components/RegistroEntradaSaida2'
+
 export default function HistoricoEstoqueScreen({navigation}){
 
     const historyStock = useSelector(({estoque}) => estoque.all)
-    const dispatch = useDispatch()
+	const dispatch = useDispatch()
+
     useEffect(()=>
         navigation.addListener('focus', () => {
             dispatch(activateSearchAction())
             dispatch(setSearch('estoque'))
         })
     ,[navigation])
-    useEffect(()=>{
+
+	useEffect(()=>{
         dispatch(getHistoryStock())
     },[dispatch])
 
-    const stockHistoryItem = ({modo, preco, quantidade, createdAt, produto_id, produto})=>{
-        return(
-        <View key={produto_id} style={modo == 'entrada'?styles.stockItemContainerEntrada:styles.stockItemContainerSaida}>
-            <View style={styles.dataItemContainer}>
-                <Text style={styles.textItemContainer}>{date(createdAt)}</Text>
-                <Text style={modo == 'entrada'?styles.modoEntrada:styles.modoSaida}>{modo}</Text>
-                <Text style={styles.textItemContainer}>{preco} reais</Text>
-            </View>
-            <ImageBackground style={ styles.dataItemSecondContainer } blurRadius={5} borderRadius={22} source={getImage(produto.imagem)}>
-                <Text style={styles.textItemContainerWhite}>{produto.nome}</Text>
-                <Text style={styles.textItemContainerWhite}>{quantidade} unidades</Text>
-            </ImageBackground>
-        </View>
-    )}
+	function handleCarregarDados(){dispatch(getHistoryStock())}
+
     return (
         <>
-        <Text>{historyStock?"":JSON.stringify(historyStock)}</Text>
-            <FlatList
-                style={{borderColor:'red', borderWidth:2}}
-                data={historyStock}
-                renderItem={({item}) => stockHistoryItem(item)}
-                keyExtractor={(item, index)=> String(index)}
-            />
+			{historyStock.length?(
+				<FlatList
+				style={{borderColor:'red', borderWidth:2}}
+					data={historyStock}
+					renderItem={({item}) => {
+						const {modo, preco, quantidade, createdAt, produto_id, produto} = item
+						return <RegistroEntradaSaida2 modo={modo}
+						produto_id={produto_id}
+									preco={preco}
+									label1_box={produto.nome}
+									label2_box={`${quantidade} unidades`}
+									nome_imagem={produto.imagem}
+									data={createdAt}
+									/>
+								}}
+
+								keyExtractor={(item, index)=> String(index)}
+				/>
+				):(
+					<View style={{justifyContent:'center', flex:1}}>
+						<Button  color="red" title="Carregar Dados" onPress={handleCarregarDados}/>
+					</View>
+				)}
         </>
     )
 }
