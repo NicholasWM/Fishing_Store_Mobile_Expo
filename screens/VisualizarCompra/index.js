8dc14ba5afda3331f1f3d8c55e50e30ab629c535
NicholasWM <nicholaswesleymazzei@gmail.com>
2020-05-18 19:37:44 -0300

    screen: Informaçoes caixa em Visualizar Compras

--- screens/VisualizarCompra/index.js
+++ screens/VisualizarCompra/index.js
@@ -1,11 +1,11 @@
 import React, {useEffect} from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 const barco_imagem = require('../../assets/images/barco.png')
 const grupo_imagem = require('../../assets/images/grupo.png')
 import ValoresCaixa from '../../components/ValoresCaixa'
 import {useSelector, useDispatch} from 'react-redux'
 import {fetchLivroCaixaDadosCompraSelecionada} from '../../store/fetchActions'
-const {testeStyle} = require('../../helpers/Style')
+const {testeStyle, textShadow} = require('../../helpers/Style')
 export default function VisualizarCompra({route, navigation}){
 	const { id, nome, barqueiro, produtos, preco_total } = route.params
 	const dadosLivroCaixa = useSelector(({livro_caixa}) => livro_caixa.compra_selecionada)
@@ -14,22 +14,48 @@ export default function VisualizarCompra({route, navigation}){
 	useEffect(()=> {
 		dispatch(fetchLivroCaixaDadosCompraSelecionada(id))
 	},[])
-	return (
-		<View style={{height:'100%', ...testeStyle}}>
-			{/* <Text>{JSON.stringify(route.params)}</Text>
-			<Text>{JSON.stringify(dadosLivroCaixa)}</Text> */}
-			<View style={{height:"100%", ...testeStyle}}>
+
+	const Caixa = () => {
+		const styles = StyleSheet.create({
+			container:{height:"100%", ...testeStyle},
+			itemContainer: {
+				flexDirection:'row', alignItems:'center', justifyContent:'space-around',
+				backgroundColor:"#D60800", padding:20,
+				borderColor:'black', borderWidth:2,
+				width:"50%", marginTop:1
+			},
+			containerResumoText: {
+				color:'#FFF', fontSize: 20,...textShadow
+			},
+			containerResumo: {
+				width:"100%",
+				flexDirection:'row'
+			}
+		})
+		return (
+			<View style={styles.container}>
 				<ValoresCaixa
-					// style={testeStyle}
 					credito={dadosLivroCaixa.credito != undefined ? dadosLivroCaixa.credito.reduce((acc, curr)=> acc + curr.valor, 0): "loading"}
 					debito={dadosLivroCaixa.debito != undefined ? dadosLivroCaixa.debito.reduce((acc, curr)=> acc + curr.valor, 0): "loading"}
 					dinheiro={dadosLivroCaixa.dinheiro != undefined ? dadosLivroCaixa.dinheiro.reduce((acc, curr)=> acc + curr.valor, 0): "loading"}
 				/>
-				<View style={{flexDirection:'row', alignItems:'center', justifyContent:'space-around', width:"100%"}}>
-					<Text style={testeStyle}>Total: {dadosLivroCaixa.total != undefined ? dadosLivroCaixa.total.preco_total: "loading"}</Text>
-					<Text style={testeStyle}>Pago: {dadosLivroCaixa.total != undefined ? dadosLivroCaixa.total.pago: "loading"}</Text>
+
+				<View style={styles.containerResumo}>
+					<View style={styles.itemContainer}>
+						<Text style={styles.containerResumoText}>Total: {dadosLivroCaixa.total != undefined ? dadosLivroCaixa.total.preco_total: "loading"}</Text>
+					</View>
+					<View style={styles.itemContainer}>
+						<Text style={styles.containerResumoText}>Pago: {dadosLivroCaixa.total != undefined ? dadosLivroCaixa.total.pago: "loading"}</Text>
+					</View>
 				</View>
 			</View>
+		)
+	}
+	return (
+		<View style={{height:'100%', ...testeStyle}}>
+			{/* <Text>{JSON.stringify(route.params)}</Text>
+			<Text>{JSON.stringify(dadosLivroCaixa)}</Text> */}
+			{Caixa()}
 		</View>
 	)
 }
