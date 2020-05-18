b77f05c096ac4130e9b5ddb50e78706b70dea252
NicholasWM <nicholaswesleymazzei@gmail.com>
2020-05-18 19:36:37 -0300

    add: Text Shadow

--- helpers/Style/index.js
+++ helpers/Style/index.js
@@ -1,6 +1,7 @@
 const testar_estilos = 1
-module.exports ={ 
+module.exports ={
     testeStyle: ((ligar) => ligar ? { borderColor: "red", borderWidth: 1 } : {})(testar_estilos),
     iconImage:{height:30, width:30},
-    colorPallet:["#EFFFCD","#DCE9BE","#555152","#2E2633","#FF5203", "#78FF8F", "#DF7DF0", "#3086FF"]
+	colorPallet:["#EFFFCD","#DCE9BE","#555152","#2E2633","#FF5203", "#78FF8F", "#DF7DF0", "#3086FF"],
+	textShadow: {textShadowColor:"black",textShadowOffset: {width: 1, height:1}, textShadowRadius:5}
 }
