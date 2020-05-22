3519743f2a36b37c1e1231537eb16330c0f13509
NicholasWM <nicholaswesleymazzei@gmail.com>
2020-05-21 22:45:35 -0300

    feat: add Debugger

--- store/index.js
+++ store/index.js
@@ -1,4 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {composeWithDevTools} from 'remote-redux-devtools'
+
+import {createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
 import produtos from './ducks/produtos'
 import estoque from './ducks/estoque'
@@ -6,8 +9,13 @@ import search from './ducks/search'
 import livro_caixa from './ducks/livro_caixa'
 import compras from './ducks/compras'
 
-export default configureStore({
-    reducer:{
-        produtos, estoque, search, livro_caixa, compras
-    }
+const composeEnhancers = composeWithDevTools({ hostname: 'localhost', realtime: true, port: 8000 });
+
+const rootReducer = combineReducers({
+	produtos, estoque, search, livro_caixa, compras
 })
+
+export default createStore(rootReducer, composeEnhancers(
+		applyMiddleware(thunk)
+	)
+)
